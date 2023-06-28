e2d2041fef27885fdfe61edcdc10f867c7476902
Mattheus <mattheusp382@gmail.com>
2023-06-27 21:10:06 -0300

    commit_vALL

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
-const horas = document.elementByid('horas');
-const minutos = document.elementByid('minutos');
-const segundos = document.elementByid('segundos');
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
     let hr = dateToday.getHours();
     let min = dateToday.getMinutes();
-    let scdns = dateToday.getSeconds();
+    let s = dateToday.getSeconds();
 
     if (hr < 10) hr = '0' + hr;
 
-    if (min < 10) hr = '0' + min;
+    if (min < 10) min = '0' + min;
 
-    if (scnds < 10) hr = '0' + scnds;
+    if (hr < 10) hr = '0' + hr;
 
     horas.textContent = hr;
     minutos.textContent = min;
-    segundos.textContet - scdns;
+    segundos.textContent = s;
 })
